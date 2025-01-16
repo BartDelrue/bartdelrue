bd9c1ba7f403aaa27c6196fe40c89072acbffee5
bart.delrue <bart.delrue@odisee.be>
2025-01-16 12:57:52 +0100

    first draft portfolio v2

--- balls/main.js
+++ balls/main.js
@@ -3,83 +3,83 @@ import hsl2rgb from "./hsl2rgb.mjs";
 import {calcRatio} from "./contrast.mjs";
 import Stripe from "./stripe.mjs";
 
-(function () {
-    const controls = document.querySelector('#controls');
-    const btnCircle = document.querySelector('#btnCircle');
-    const btnStripe = document.querySelector('#btnStripe');
-    const css = document.querySelector('#css');
-    const baseColorInput = document.querySelector('#base');
-    const bgCollection = {};
-    const root = document.querySelector(':root');
 
-    let count = 0;
-    let color = hsl2rgb(Math.floor(Math.random() * 360) + 1, 100, 15);
-    let background = 'none';
+const controls = document.querySelector('#controls');
+const btnCircle = document.querySelector('#btnCircle');
+const btnStripe = document.querySelector('#btnStripe');
+const css = document.querySelector('#css');
+const baseColorInput = document.querySelector('#base');
+const bgCollection = {};
+const root = document.querySelector(':root');
 
-    setBackground(color);
+let count = 0;
+let color = hsl2rgb(Math.floor(Math.random() * 360) + 1, 100, 15);
+let background = 'none';
 
-    baseColorInput.value = color;
-    baseColorInput.addEventListener('change', ({target: {value}}) => setBackground(value));
-    btnCircle.addEventListener('click', () => {
-        const uid = ++count;
-        new Ball(uid,
-            undefined,
-            c => controls.appendChild(c),
-            draw,
-            c => controls.removeChild(c));
-    });
-    btnStripe.addEventListener('click', () => {
-        const uid = ++count;
-        new Stripe(uid,
-            undefined,
-            c => controls.appendChild(c),
-            draw,
-            c => controls.removeChild(c));
-    });
+setBackground(color);
 
-    function setBackground(v) {
-        color = v;
-        root.style.setProperty('--bg-color', color)
-        root.style.setProperty('--text-color', calcRatio(color, '#FFFFFF') < 4.5 ? '#000000' : '#FFFFFF');
-        root.style.setProperty('--shadow-color', calcRatio(color, '#FFFFFF') < 4.5 ? '#FFFFFF' : '#000000');
-        showValue()
-    }
+baseColorInput.value = color;
+baseColorInput.addEventListener('change', ({target: {value}}) => setBackground(value));
+btnCircle.addEventListener('click', () => {
+    const uid = ++count;
+    new Ball(uid,
+        undefined,
+        c => controls.appendChild(c),
+        draw,
+        c => controls.removeChild(c));
+});
+btnStripe.addEventListener('click', () => {
+    const uid = ++count;
+    new Stripe(uid,
+        undefined,
+        c => controls.appendChild(c),
+        draw,
+        c => controls.removeChild(c));
+});
 
-    function draw(uid, bg) {
-        bg ? bgCollection[uid] = bg : delete bgCollection[uid]
-        background = `${Object.values(bgCollection).toString()}`
-        root.style.setProperty('--bg', "" + background);
-        showValue()
-    }
+function setBackground(v) {
+    color = v;
+    root.style.setProperty('--bg-color', color)
+    root.style.setProperty('--text-color', calcRatio(color, '#FFFFFF') < 4.5 ? '#000000' : '#FFFFFF');
+    root.style.setProperty('--shadow-color', calcRatio(color, '#FFFFFF') < 4.5 ? '#FFFFFF' : '#000000');
+    showValue()
+}
 
-    function showValue() {
-        css.value = `background-color: ${color};\nbackground-image: ${background};`;
-    }
+function draw(uid, bg) {
+    bg ? bgCollection[uid] = bg : delete bgCollection[uid]
+    background = `${Object.values(bgCollection).toString()}`
+    root.style.setProperty('--bg', "" + background);
+    showValue()
+}
 
-    for (let i = 3; i--;) {
-        new Ball(++count,
-            {
-                x: (Math.floor(Math.random() * 200) + 1) - 100,
-                y: (Math.floor(Math.random() * 200) + 1) - 100,
-                alpha: Math.floor((Math.random() * 30)) + 20,
-                size: (Math.floor(Math.random() * 20) + 41),
-                blur: 0
-            },
-            c => controls.appendChild(c),
-            draw,
-            c => controls.removeChild(c))
-    }
+function showValue() {
+    css.value = `background-color: ${color};\nbackground-image: ${background};`;
+}
 
-    new Stripe(++count,
+for (let i = 3; i--;) {
+    new Ball(++count,
         {
             x: (Math.floor(Math.random() * 200) + 1) - 100,
             y: (Math.floor(Math.random() * 200) + 1) - 100,
             alpha: Math.floor((Math.random() * 30)) + 20,
             size: (Math.floor(Math.random() * 20) + 41),
-            blur: 0,
-            angle: (Math.floor(Math.random() * 720) + 1) - 360
+            blur: 0
         },
         c => controls.appendChild(c),
         draw,
-        c => controls.removeChild(c));
-})();
+        c => controls.removeChild(c))
+}
+
+new Stripe(++count,
+    {
+        x: (Math.floor(Math.random() * 200) + 1) - 100,
+        y: (Math.floor(Math.random() * 200) + 1) - 100,
+        alpha: Math.floor((Math.random() * 30)) + 20,
+        size: (Math.floor(Math.random() * 20) + 41),
+        blur: 0,
+        angle: (Math.floor(Math.random() * 720) + 1) - 360
+    },
+    c => controls.appendChild(c),
+    draw,
+    c => controls.removeChild(c));
+
